0f62888236eed6a7a8622cb5058682d4e9d4c8be
MajorThemes <hello@major-themes.com>
2016-08-20 00:03:13 +0300

    Init

---
+++ mjr-tools.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: MJR Tools
+Plugin URI: https://major-themes.com/plugins/widgets
+Description: Bunch of useful Widgets
+Version: 1.0.0
+Author: MajorThemes
+Author URI: https://major-themes.com
+*/
+
+include( plugin_dir_path( __FILE__ ) . 'mjr-profile-widget.php');
+include( plugin_dir_path( __FILE__ ) . 'mjr-post-widget.php');
